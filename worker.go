f04ae3b550a92e195588006cd5c92924a32d0f47
Dmitry Shalashov <skaurus@gmail.com>
2022-08-01 23:44:01 +0300

    fix: probably fixed it even more

--- worker.go
+++ worker.go
@@ -52,7 +52,7 @@ func (w *worker) start(c *redis.Client, job *Job) error {
 		return err
 	}
 
-	err = c.Set(context.Background(), fmt.Sprintf("%sworker:%s", workerSettings.Namespace, w.process), buffer, 0).Err()
+	err = c.Set(context.Background(), fmt.Sprintf("%sworker:%s", workerSettings.Namespace, w.process.String()), buffer, 0).Err()
 	if err != nil {
 		return err
 	}
