d6c3dfd2d290da2b440a75f0596ffb1cf61dd689
Dmitry Shalashov <skaurus@gmail.com>
2022-08-14 02:36:30 +0400

    feat: previous solution did not work

--- worker_func.go
+++ worker_func.go
@@ -2,4 +2,4 @@ package goworker
 
 type workerFunc func(queue string, tasks ...interface{}) error
 
-type decoderFunc func(job string) (class string, args []interface{}, err error)
+type decoderFunc func(job string) (class string, args interface{}, err error)
